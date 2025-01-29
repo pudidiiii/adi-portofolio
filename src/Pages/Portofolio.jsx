f7602a5fd6f9bfd1937f5a2e88f49342b1a97ad0
pudidiiii <adi383903@gmail.com>
2025-01-29 16:56:01 +0700

    Update Portofolio.jsx

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -101,7 +101,7 @@ function a11yProps(index) {
 }
 
 const techStacks = [
-  { icon: "/public/Ai.svg", language: "Adobe Illustrator" },
+  { icon: "/Ai1.svg", language: "Adobe Illustrator" },
   { icon: "/dist/pr.svg", language: "Adobe Premier Pro" },
   { icon: "/dist/ae.svg", language: "Adobe After Effect" },
   { icon: "/dist/ps.svg", language: "Adobe Photoshop" },
